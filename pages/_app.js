509b7a49f13bc99341abc77918d904beb7c10106
sagarwebenix <87569098+sagarwebenix@users.noreply.github.com>
2022-02-19 16:58:59 +0530

    first change

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,21 @@ import { GlobalStyle } from "../styles/global/global.style";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/global/theme";
 import Layout from "../components/Layout";
+
 function MyApp({ Component, pageProps }) {
-  return <> 
-      	<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>      
-      	</ThemeProvider>
-    </>
+    return < >
+        <
+        ThemeProvider theme = { theme } >
+        <
+        GlobalStyle / >
+        <
+        Layout >
+        <
+        Component {...pageProps }
+    /> < /
+    Layout > <
+        /ThemeProvider> < /
+        >
 }
 
-export default MyApp
+export default MyApp;
